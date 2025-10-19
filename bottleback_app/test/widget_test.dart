e1fe6e5794658cef6f87cb04b7d200af0b2962bc
Mistergunth <sirichokhkalyavththi@gmail.com>
2025-10-19 14:18:36 +0700

    add firebase

--- bottleback_app/test/widget_test.dart
+++ bottleback_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bottleback_app/main.dart';
+import 'package:bottleback_app/main_user.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
